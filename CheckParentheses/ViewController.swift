099efb5a445c4abc26afac367e8aa901c252c90d
Alex Tikhomirov <alexandrtikhomirov@gmail.com>
2018-01-23 14:26:23 +0200

    First commit

---
+++ CheckParentheses/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  CheckParentheses
+//
+//  Created by  Tikhomirov on 23.01.2018.
+//  Copyright © 2018 SHT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet private weak var textView: UITextView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        textView.delegate = self
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    // MARK: - Private methods
+    private func changeText(_ textView: UITextView) {
+        guard let text = textView.text else { return }
+        var leftCount = 0
+        var rightCount = 0
+        var flagError = false
+        for item in text {
+            if item == "(" { leftCount += 1 }
+            if item == ")" { leftCount <= rightCount ? (flagError = true) : (rightCount += 1) }
+        }
+        flagError ? (textView.textColor = UIColor.red) : (textView.textColor = UIColor.black)
+    }
+}
+
+// MARK: - UITextViewDelegate
+extension ViewController: UITextViewDelegate {
+    
+    func textViewDidChange(_ textView: UITextView) {
+        changeText(textView)
+    }
+}
